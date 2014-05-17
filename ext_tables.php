0e207b489de0adedd2ec2c7203f4da79e4c6b805
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-05-17 16:55:01 +0200

    [BUGFIX] Use Static Template the conventional way
    Stop being a rogue!

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,7 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Fluid Pages: Bootstrap package');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Speciality: main template');
 
 # Add page TSConfig
 $pageTsConfig = \TYPO3\CMS\Core\Utility\GeneralUtility::getUrl(
