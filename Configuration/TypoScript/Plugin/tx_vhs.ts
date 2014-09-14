737a6d3c4b36d367258840c1377e20a7be74e586
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-09-14 21:58:50 +0200

    [TASK] Include assets loading by TS

---
+++ Configuration/TypoScript/Plugin/tx_vhs.ts
@@ -0,0 +1,21 @@
+###################################################
+# Plugin VHS
+###################################################
+
+plugin.tx_vhs.settings.asset {
+	mainCss {
+		path = EXT:speciality/Resources/Public/Build/StyleSheets/site.min.css
+		group = css
+		standalone = 1
+		rewrite = 0
+		type = css
+	}
+
+	mainJs {
+		path = EXT:speciality/Resources/Public/Build/JavaScript/site.min.js
+		group = js
+		standalone = 1
+		rewrite = 0
+		type = js
+	}
+}
