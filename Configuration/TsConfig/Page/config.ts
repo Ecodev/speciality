4cb049d88a1ef64801d66be910584ea4dd7c6260
Fabien Udriot <fabien.udriot@ecodev.ch>
2014-11-07 10:40:15 +0100

    [BUGFIX] Page permissions
    Resolves #113

--- Configuration/TsConfig/Page/config.ts
+++ Configuration/TsConfig/Page/config.ts
@@ -6,14 +6,18 @@ mod.SHARED {
 }
 
 # PAGE DEFAULT PERMISSIONS
+
+# Please confirm or infirm on github.com/Ecodev/typo3-cms-speciality-distribution/issues
+# Some people report, it does not work unless this snippet is put on the root page TSconfig.
+# @see https://github.com/Ecodev/typo3-cms-speciality-distribution/issues/113
 TCEMAIN.permissions {
 
 	# Configure default permission for new created pages.
 	# do anything (default):
-	user = 31
+	user = show, editcontent, edit, new, delete
 
 	# do anything (normally "delete" is disabled)
-	group = 31
+	group = show, editcontent, edit, new, delete
 
 	# (normally everybody can do nothing)
 	everybody =
