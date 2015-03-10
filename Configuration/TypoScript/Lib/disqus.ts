df88eced615dbfefe475c74d0c8f6dc24714a85e
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-03-10 15:25:25 +0100

    [TASK] Remove Disqus configuration

--- Configuration/TypoScript/Lib/disqus.ts
+++
@@ -1,9 +0,0 @@
-###################################################
-# Disqus
-###################################################
-
-[PIDupinRootline = 82] || [globalVar = TSFE:id = 1]
-	lib.disqus.display = 1
-[else]
-	lib.disqus.display = 0
-[end]
