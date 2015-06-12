c37337483e57717452d5e371de3f54cbca7f6e6e
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-06-12 21:28:58 +0200

    [TASK] Streamline default configuration for IndexedSearch
    Resolves #15

--- Configuration/TypoScript/Plugin/tx_indexedsearch.ts
+++ Configuration/TypoScript/Plugin/tx_indexedsearch.ts
@@ -3,35 +3,11 @@
 ###################################################
 
 plugin.tx_indexedsearch {
-
+	# Override template path configuration
 	templateFile = EXT:speciality/Resources/Private/Plugins/IndexedSearch/SearchForm.html
 	_CSS_DEFAULT_STYLE >
-	_DEFAULT_PI_VARS.results = 10
-	forwardSearchWordsInResultLink = 1
-	blind {
-		type=-1
-		defOp=0
-		sections=0
-		media=1
-		order=-1
-		group=-1
-		extResume=-1
-		lang=-1
-		desc=-1
-		results=0
-	}
-	show {
-		rules=0
-		parsetimes=1
-		L2sections=1
-		L1sections=1
-		LxALLtypes=0
-		clearSearchBox = 0
-		clearSearchBox.enableSubSearchCheckBox=0
-		advancedSearchLink = 0
-	}
 	search {
-		rootPidList =
+		targetPid >
+		targetPid = 151
 	}
-	#templateFile = fileadmin/templates/plugins/indexed_search/searchresult.html
 }
