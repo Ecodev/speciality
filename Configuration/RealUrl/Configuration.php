d85df6f797c73da9c8d7ff9dae2c59429116b408
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-10-20 11:41:55 +0200

    [TASK] Streamline RealURL default configuration
    Resolves #21

--- Configuration/RealUrl/Configuration.php
+++ Configuration/RealUrl/Configuration.php
@@ -3,15 +3,6 @@
  * RealURL helper script
  ***************************/
 
-/**
- * Prevent new realurl updates from clearing cache
- */
-unset($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearAllCache_additionalTables']['tx_realurl_pathcache']);
-
-/**
- * Realurl naming precedence configuration
- */
-$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_realurl_pathsegment,alias,title';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array();
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array(
 	'init' => array(
@@ -96,117 +87,6 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array(
 				array('GETvar' => 'tx_indexedsearch[order]'),
 				array('GETvar' => 'tx_indexedsearch[freeIndexUid]')
 			),
-			// ab_downloads,
-			'dl-act' => array(
-				array(
-					'GETvar' => 'tx_abdownloads_pi1[action]',
-					'valueMap' => array(
-						'show-category' => 'getviewcategory',
-						'propose-a-new-download' => 'getviewaddnewdownload',
-						'open-download' => 'getviewclickeddownload',
-						'show-details-for-download' => 'getviewdetailsfordownload',
-						'report-broken-download' => 'getviewreportbrokendownload',
-						'rate-download' => 'getviewratedownload',
-					),
-				),
-			),
-			'dl-cat' => array(
-				array(
-					'GETvar' => 'tx_abdownloads_pi1[category_uid]',
-					'valueMap' => array(
-						'home' => '0',
-					),
-					'lookUpTable' => array(
-						'table' => 'tx_abdownloads_category',
-						'id_field' => 'uid',
-						'alias_field' => 'label',
-						'addWhereClause' => ' AND deleted != 1',
-						'useUniqueCache' => 1,
-						'autoUpdate' => 1,
-						'useUniqueCache_conf' => array(
-							'strtolower' => 1,
-							'spaceCharacter' => '-',
-						),
-					),
-				),
-			),
-			'dl-file' => array(
-				array(
-					'GETvar' => 'tx_abdownloads_pi1[uid]',
-					'lookUpTable' => array(
-						'table' => 'tx_abdownloads_download',
-						'id_field' => 'uid',
-						'alias_field' => 'label',
-						'addWhereClause' => ' AND deleted != 1',
-						'useUniqueCache' => 1,
-						'autoUpdate' => 1,
-						'useUniqueCache_conf' => array(
-							'strtolower' => 1,
-							'spaceCharacter' => '-',
-						),
-					),
-				),
-			),
-			'dl-ptr' => array(
-				array(
-					'GETvar' => 'tx_abdownloads_pi1[pointer]',
-				),
-			),
-			'll-act' => array(
-				array(
-					'GETvar' => 'tx_ablinklist_pi1[action]',
-					'valueMap' => array(
-						'show-category' => 'getviewcategory',
-						'propose-a-new-link' => 'getviewaddnewlink',
-						'open-link' => 'getviewclickedlink',
-						'show-details-for-link' => 'getviewdetailsforlink',
-						'report-broken-link' => 'getviewreportbrokenlink',
-						'rate-link' => 'getviewratelink',
-					),
-				),
-			),
-			'll-cat' => array(
-				array(
-					'GETvar' => 'tx_ablinklist_pi1[category_uid]',
-					'valueMap' => array(
-						'home' => '0',
-					),
-					'lookUpTable' => array(
-						'table' => 'tx_ablinklist_category',
-						'id_field' => 'uid',
-						'alias_field' => 'label',
-						'addWhereClause' => ' AND deleted != 1',
-						'useUniqueCache' => 1,
-						'autoUpdate' => 1,
-						'useUniqueCache_conf' => array(
-							'strtolower' => 1,
-							'spaceCharacter' => '-',
-						),
-					),
-				),
-			),
-			'll-link' => array(
-				array(
-					'GETvar' => 'tx_ablinklist_pi1[uid]',
-					'lookUpTable' => array(
-						'table' => 'tx_ablinklist_link',
-						'id_field' => 'uid',
-						'alias_field' => 'label',
-						'addWhereClause' => ' AND deleted != 1',
-						'useUniqueCache' => 1,
-						'autoUpdate' => 1,
-						'useUniqueCache_conf' => array(
-							'strtolower' => 1,
-							'spaceCharacter' => '-',
-						),
-					),
-				),
-			),
-			'll-ptr' => array(
-				array(
-					'GETvar' => 'tx_ablinklist_pi1[pointer]',
-				),
-			),
 			'article' => array(
 				array(
 					'GETvar' => 'tx_news_pi1[news]',
@@ -222,17 +102,6 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array(
 						),
 					),
 				),
-				# @todo check how controller and action can be hidden from URL. This configuration used to work prior to 6.1.
-				#array(
-				#	'GETvar' => 'tx_news_pi1[controller]',
-				#	'noMatch' => 'null',
-				#	'default' => 'News',
-				#),
-				#array(
-				#	'GETvar' => 'tx_news_pi1[action]',
-				#	'noMatch' => 'null',
-				#	'default' => 'detail',
-				#),
 			),
 		)
 	),
@@ -241,36 +110,6 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl']['_DEFAULT'] = array(
 			'index.html' => array(
 				'keyValues' => array(
 					'type' => 0,
-				)
-			),
-			'print.html' => array(
-				'keyValues' => array(
-					'type' => 98,
-				)
-			),
-			'text.html' => array(
-				'keyValues' => array(
-					'type' => 99,
-				)
-			),
-			'rss.xml' => array(
-				'keyValues' => array(
-					'type' => 100,
-				)
-			),
-			'rss091.xml' => array(
-				'keyValues' => array(
-					'type' => 101,
-				)
-			),
-			'rdf.xml' => array(
-				'keyValues' => array(
-					'type' => 102,
-				)
-			),
-			'atom.xml' => array(
-				'keyValues' => array(
-					'type' => 103,
 				)
 			),
 			// ext:seo_basics overrides this,
@@ -348,5 +187,3 @@ if (false) {
 		)
 	);
 }
-
-?>
