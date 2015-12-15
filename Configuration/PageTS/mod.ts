35cb1c2204473869918654f008b6ce8b88f37267
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-12-15 15:19:12 +0100

    [TASK] Hide some content element from wizard for non-admin users only
    Resolves #29

--- Configuration/PageTS/mod.ts
+++ Configuration/PageTS/mod.ts
@@ -8,22 +8,25 @@ mod.SHARED {
 }
 
 
-# Re-organize tabs within the new Content Element wizard
+# Re-organize tabs within the new Content Element wizard only for regular user.
+[adminUser = 0]
 
-# no tabs as the list is fairly small.
-mod.wizards.newContentElement.renderMode = NoTabs
+  # no tabs as the list is fairly small.
+  mod.wizards.newContentElement.renderMode = NoTabs
 
-# 1. common
-mod.wizards.newContentElement.wizardItems.common.show = text, image
+  # 1. common
+  mod.wizards.newContentElement.wizardItems.common.show = text, image
 
-# 2. special
-mod.wizards.newContentElement.wizardItems.special.show >
+  # 2. special
+  mod.wizards.newContentElement.wizardItems.special.show >
 
-# 3. forms
-mod.wizards.newContentElement.wizardItems.forms.show >
+  # 3. forms
+  mod.wizards.newContentElement.wizardItems.forms.show >
 
-# 4. plugins
-mod.wizards.newContentElement.wizardItems.plugins.show >
+  # 4. plugins
+  mod.wizards.newContentElement.wizardItems.plugins.show >
 
-# 5. Bootstrap
-mod.wizards.newContentElement.wizardItems.Bootstrap.show = FluidBT_Fluidbootstraptheme_Alert_html, FluidBT_Fluidbootstraptheme_Accordion_html, FluidBT_Fluidbootstraptheme_ImageGallery_html, FluidBT_Fluidbootstraptheme_Carousel_html, FluidBT_Fluidbootstraptheme_SimpleResponsiveImage_html
+  # 5. Bootstrap
+  mod.wizards.newContentElement.wizardItems.Bootstrap.show = FluidBT_Fluidbootstraptheme_Alert_html, FluidBT_Fluidbootstraptheme_Accordion_html, FluidBT_Fluidbootstraptheme_ImageGallery_html, FluidBT_Fluidbootstraptheme_Carousel_html, FluidBT_Fluidbootstraptheme_SimpleResponsiveImage_html
+
+[global]
