df52cfdd28dc659634843879c89ed8b79c348841
Fabien Udriot <fabien.udriot@ecodev.ch>
2016-06-15 20:19:05 +0200

    [TASK] Update FluidTYPO3 migration script

--- class.ext_update.php
+++ class.ext_update.php
@@ -1,17 +1,11 @@
 <?php
 namespace Ecodev\Speciality;
 
-/**
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
+/*
+ * This file is part of the Ecodev\Speciality project under GPLv2 or later.
  *
  * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
+ * LICENSE.md file that was distributed with this source code.
  */
 
 /**
@@ -34,7 +28,61 @@ class ext_update
     public function main()
     {
 
-        $result = 'Extension "fluidbootstraptheme" is still enabled. I will  not do anything!';
+        $lines[] = $this->updatePagesTemplate();
+        $lines[] = $this->updateContentTemplatefile();
+        return implode('<br/>', $lines);
+    }
+
+    /**
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    private function updatePagesTemplate()
+    {
+
+        // Fetch records that needs to be updated
+        $records = $this->getDatabaseConnection()->exec_SELECTgetRows(
+            'uid, tx_fed_page_controller_action, tx_fed_page_controller_action_sub',
+            'pages',
+            'tx_fed_page_controller_action != "" OR tx_fed_page_controller_action_sub != "" AND deleted = 0'
+        );
+
+        $counter = 0;
+        foreach ($records as $record) {
+
+            $values = [];
+            if (preg_match('/speciality->/', $record['tx_fed_page_controller_action'])) {
+                $parts = explode('->', $record['tx_fed_page_controller_action']);
+                $values['tx_fed_page_controller_action'] = ucfirst($parts[0]) . '->' . lcfirst($parts[1]);
+            }
+
+            if (preg_match('/speciality->/', $record['tx_fed_page_controller_action_sub'])) {
+                $parts = explode('->', $record['tx_fed_page_controller_action_sub']);
+                $values['tx_fed_page_controller_action_sub'] = ucfirst($parts[0]) . '->' . lcfirst($parts[1]);
+            }
+
+            if (count($values) > 0) {
+                $counter++;
+                $this->getDatabaseConnection()->exec_UPDATEquery('pages', 'uid = ' . $record['uid'], $values);
+            }
+        }
+
+        if ($counter > 0) {
+            $result = sprintf('- %s page(s) have been updated fields "tx_fed_page_controller_action" and "tx_fed_page_controller_action_sub".', count($records));
+        } else {
+            $result = '- After checking no update was necessary in pages.';
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    private function updateContentTemplatefile()
+    {
+        $result = '- Extension "fluidbootstraptheme" is still enabled. I will  not do anything!';
         if (!\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('fluidbootstraptheme')) {
 
             $records = $this->getDatabaseConnection()->exec_SELECTgetRows('uid, tx_fed_fcefile', 'tt_content', 'tx_fed_fcefile LIKE "FluidBT.Fluidbootstraptheme%"');
@@ -45,13 +93,13 @@ class ext_update
                 $this->getDatabaseConnection()->exec_UPDATEquery('tt_content', 'uid = ' . $record['uid'], $values);
             }
 
-
             if (count($records) > 0) {
-                $result = sprintf('%s record(s) have been updated with value "FluidBT.Fluidbootstraptheme" by "speciality" in field "tx_fed_fcefile".', count($records));
+                $result = sprintf('- %s record(s) have been updated with value "FluidBT.Fluidbootstraptheme" by "speciality" in field "tx_fed_fcefile".', count($records));
             } else {
-                $result = 'After checking no update was necessary.';
+                $result = '- After checking no update was necessary in tt_content.';
             }
         }
+
         return $result;
     }
 
@@ -66,4 +114,3 @@ class ext_update
     }
 
 }
-
