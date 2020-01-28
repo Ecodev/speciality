5753998a41fb67df9ebab6842a6e9a4d11b1d9a0
Fabien Udriot <fabien@omic.ch>
2020-01-28 23:09:49 +0100

    TYPO3 v9 migration

--- ext_tables.php
+++ ext_tables.php
@@ -19,23 +19,3 @@ TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
     RTE.default.preset = custom
 
 ');
-
-// New icons for the BE
-if (TYPO3_MODE === 'BE') {
-
-    $icons = array('category', 'comment', 'storage', 'news', 'people');
-    foreach ($icons as $icon) {
-
-        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class)->registerIcon(
-            'tcarecords-pages-contains-' . $icon,
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:speciality/Resources/Public/Backend/Icons/' . $icon . '.png']
-        );
-
-        $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = array(
-            ucfirst($icon),
-            $icon,
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('speciality') . 'Resources/Public/Backend/Icons/' . $icon . '.png'
-        );
-    }
-}
