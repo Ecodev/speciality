25b8b0539b489e660a74d294e6ffea81ded12f49
Fabien Udriot <fabarea@users.noreply.github.com>
2020-04-08 21:36:21 +0200

    Update ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -1,21 +1,23 @@
 <?php
-
 if (!defined('TYPO3_MODE')) die ('Access denied.');
+call_user_func(
+    function() {
 
-# Add user TSConfig
-$basePath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('speciality');
+        // Default User TSConfig to be added in any case.
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+            '
+            <INCLUDE_TYPOSCRIPT: source="DIR:EXT:speciality/Configuration/UserTS">
+            '
+        );
 
-// Default User TSConfig to be added in any case.
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-    <INCLUDE_TYPOSCRIPT: source="DIR:EXT:speciality/Configuration/UserTS">
-');
+        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['custom'] = 'EXT:speciality/Configuration/RTE/Custom.yaml';
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['custom'] = 'EXT:speciality/Configuration/RTE/Custom.yaml';
-
-// Default User TSConfig to be added in any case.
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-
-    # Hide the module in the BE.
-    RTE.default.preset = custom
-
-');
+        // Default User TSConfig to be added in any case.
+        TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '
+            # Hide the module in the BE.
+            RTE.default.preset = custom
+            '
+        );
+    }
+);
